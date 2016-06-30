d055b8e85f72d37b5096e59d62d73e6d7eea8b71
pevac <pevac@DSC-WIN10-04>
2016-06-30 23:39:48 +0300

    added LineManipulationDeltaAction

---
+++ SupperGraph/SupperGraph/Controls/LineManipulationDeltaAction.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Shapes;
+using Microsoft.Xaml.Interactivity;
+using SupperGraph.Models;
+
+namespace SupperGraph.Controls
+{
+    public class LineManipulationDeltaAction:DependencyObject, IAction
+    {
+        public object Execute(object sender, object parameter)
+        {
+            var arg = (ManipulationDeltaRoutedEventArgs) parameter;
+            var line = (Line) arg.OriginalSource;
+            var edge = (Edge) line.DataContext;
+
+            edge.LeftNode.X = edge.LeftNode.X + arg.Delta.Translation.X;
+            edge.LeftNode.Y = edge.LeftNode.Y + arg.Delta.Translation.Y;
+
+            edge.RightNode.X = edge.RightNode.X + arg.Delta.Translation.X;
+            edge.RightNode.Y = edge.RightNode.Y + arg.Delta.Translation.Y;
+            return (object) true;
+        }
+    }
+}
---
+++ SupperGraph/SupperGraph/Controls/ToggleButtonOnManipulationDeltaAction.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Input;
+using Microsoft.Xaml.Interactivity;
+using SupperGraph.Models;
+
+namespace SupperGraph.Controls
+{
+    public class ToggleButtonOnManipulationDeltaAction:DependencyObject, IAction
+    {
+        public object Execute(object sender, object parameter)
+        {
+            var arg = (ManipulationDeltaRoutedEventArgs)parameter;
+
+            var togglebutton = (ToggleButton)arg.OriginalSource;
+            var node = (Node)togglebutton.DataContext;
+
+            node.X = node.X + arg.Delta.Translation.X;
+            node.Y = node.Y + arg.Delta.Translation.Y;
+
+            return null;
+        }
+    }
+}
